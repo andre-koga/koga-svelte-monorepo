157d0bbb6a5a7eb1a9859f3040791f0504505ad9
S Taubman <akoga3@gatech.edu>
2024-06-03 15:36:54 +0200

    added fonts

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '@fontsource/bricolage-grotesque';
+	import '@fontsource/jetbrains-mono';
+	import '@fontsource/inter';
+	import '../app.css';
+</script>
+
+<nav class="hidden md:block">header</nav>
+
+<slot />
+
+<nav class="hidden">footer</nav>
