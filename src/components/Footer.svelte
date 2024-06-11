3532ea2ee1e59ea134df667cdc168529b94bc32d
S Taubman <akoga3@gatech.edu>
2024-06-11 21:53:56 +0200

    footer header and sidebar done, just gotta add content to them

---
+++ src/components/Footer.svelte
@@ -0,0 +1,41 @@
+<footer
+	class="-m h-screen flex flex-col justify-between bg-gradient-to-b from-stone-50 pb-20 to-stone-200 px-4 pt-40 md:px-8 lg:px-12"
+>
+	<div>
+		<p class="text-sm text-stone-300 text-center pb-8">the links don't work yet!</p>
+		<p class="text-2xl font-bold rounded-sm font-display pb-6">Other Resources</p>
+		<div class="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 lg:mr-[50%] gap-4 justify-start">
+			<a href="/">Repository</a>
+			<a href="/">Related Projects</a>
+			<a href="/">Community</a>
+			<a href="/">API Reference</a>
+			<a href="/">Blog</a>
+			<a href="/">Tools and Libraries</a>
+		</div>
+	</div>
+	<div>
+		<div class="bg-stone-200 p-8 font-text my-6 rounded-lg text-sm italic text-justify">
+			"The important thing is not to stop questioning. Curiosity has its own reason for existence."
+			- Albert Einstein
+		</div>
+		<div class="bordered py-8 justify-around flex gap-4 flex-wrap text-sm">
+			<a href="/">Terms</a>
+			<a href="/">Privacy</a>
+			<a href="/">Security</a>
+			<a href="/">Docs</a>
+			<a href="/">Contact</a>
+			<a href="/">Manage Cookies</a>
+		</div>
+		<p class="text-xs text-center lowercase text-stone-500 pt-4">
+			Designed and developed by Koga himself.<br />Much love! (●'◡'●)
+		</p>
+	</div>
+</footer>
+
+<style>
+	.bordered {
+		background-image: radial-gradient(circle at 1px 1px, rgb(168 162 158) 1px, transparent 0);
+		background-size: 12px;
+		background-repeat: repeat-x;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script>
+	import { blur } from 'svelte/transition';
+
 	import '@fontsource/bricolage-grotesque';
 	import '@fontsource/bricolage-grotesque/500.css';
 	import '@fontsource/jetbrains-mono';
 	import '@fontsource/inter';
 	import '../app.css';
-	import { Menu } from 'lucide-svelte';
+	import { Menu, X } from 'lucide-svelte';
+	import Footer from '../components/Footer.svelte';
 
+	let active = false;
 	// Scroller
 	let scrollerText = 'Website still under construction ~ Please check back soon!';
 </script>
@@ -14,9 +18,70 @@
 	<!-- <div class="bg-stone-200 relative z-10 text-nowrap -m p scroller">
 		<span>{scrollerText}</span>
 	</div> -->
-	<nav class="-m p relative gap-4 flex w-screen items-center md:justify-between">
-		<button class="z-10"><Menu /></button>
+	<nav class="fixed -m p gap-4 flex w-screen items-center md:justify-between">
+		<button
+			on:click={() => (active = !active)}
+			class="z-20 h-6 w-6 relative md:hidden sidebar-button"
+		>
+			{#if active}
+				<div class="absolute top-0" transition:blur={{ delay: 0, duration: 200 }}>
+					<X />
+				</div>
+			{:else}
+				<div class="absolute top-0" transition:blur={{ delay: 0, duration: 200 }}>
+					<Menu />
+				</div>
+			{/if}
+		</button>
+		<aside class="flex flex-col justify-between" class:active>
+			<div class="flex flex-col gap-2 px-4">
+				<a class="z-10 link text-2xl font-bold rounded-sm font-display" href="/">AK .</a>
+				<p class="font-bold uppercase text-xs mt-4">Pages</p>
+				<div class="flex gap-4">
+					<a class="z-10 link" href="/about">About</a>
+					<a class="z-10 link" href="/contact">Contact</a>
+				</div>
+				<p class="font-bold uppercase text-xs mt-4">Projects</p>
+				<div class="flex flex-wrap items-center gap-4">
+					<a
+						class="z-10 link category bg-gradient-to-r from-yellow-300 to-rose-300"
+						href="/thoughts"
+						>Thoughts
+					</a>
+					<a class="z-10 link category bg-gradient-to-r from-blue-300 to-indigo-300" href="/blog">
+						Numerus
+					</a>
+				</div>
+				<p class="font-bold uppercase text-xs mt-4">Connect</p>
+				<!-- <div class="flex flex-wrap items-center gap-4">
+					<a class="z-10 link category bg-gradient-to-r from-orange-300 to-rose-400" href="/"
+						>Instagram
+					</a>
+					<a class="z-10 link category bg-gradient-to-r from-sky-300 to-blue-400" href="/">
+						Twitter
+					</a>
+					<a
+						class="z-10 link category text-white bg-gradient-to-r from-sky-950 to-slate-700"
+						href="/"
+					>
+						Github
+					</a>
+					<a
+						class="z-10 link category text-white bg-gradient-to-r from-red-500 to-red-600"
+						href="/"
+					>
+						Youtube
+					</a>
+				</div> -->
+				<!-- <div class="text-sm text-gray-300">sidebar footer stuff!</div> -->
+			</div>
+		</aside>
 		<a class="z-10 link text-2xl font-bold rounded-sm font-display" href="/">AK .</a>
+		<a class="z-10 link hidden md:block" href="/about">About</a>
+		<a class="z-10 link hidden md:block" href="/contact">Contact</a>
+		<div class="flex-grow bg-gradient-to-r from-stone-100 to-stone-200 z-10 rounded-lg">
+			<span class="opacity-0">.</span>
+		</div>
 		<a
 			class="z-10 hidden md:block link category bg-gradient-to-r from-yellow-300 to-rose-300"
 			href="/thoughts"
@@ -28,11 +93,6 @@
 		>
 			Numerus
 		</a>
-		<div class="flex-grow bg-gradient-to-r from-stone-200 to-stone-100 z-10 rounded-lg">
-			<span class="opacity-0">.</span>
-		</div>
-		<a class="z-10 link hidden md:block" href="/about">About</a>
-		<a class="z-10 link hidden md:block" href="/contact">Contact</a>
 		<div class="gradient-blur">
 			<div></div>
 			<div></div>
@@ -45,7 +105,29 @@
 
 	<slot />
 
-	<nav class="hidden md:grid">footer</nav>
-
-	<nav class="md:hidden">mobile footer</nav>
+	<Footer />
 </main>
+
+<style>
+	aside {
+		z-index: 15;
+		position: fixed;
+		top: 0;
+		left: -100vw;
+		transition: all 0.4s;
+		height: 100vh;
+		width: 100vw;
+		border: 1px solid #ddd;
+		background-color: #efefef;
+		@apply pt-16;
+	}
+	.active {
+		left: 0px;
+	}
+	.sidebar-button {
+		transition: all 0.5s;
+	}
+	.sidebar-button:active {
+		transition: all 0.1s;
+	}
+</style>
